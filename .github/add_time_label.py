cd5d73990355b77f8c7554ee554e230b410f9f1e
tbsmcd <tbsmcd@gmail.com>
2021-03-09 00:21:03 +0900

    Update add_time_label.py

--- .github/add_time_label.py
+++ .github/add_time_label.py
@@ -1,10 +1,10 @@
 import sys
+import json
 import requests
 
 args = sys.argv
 token = args[1]
-api_url = args[2] + '/labels'
-sum_seconds = int(args[3])
+sum_seconds = int(args[2])
 
 if sum_seconds <= 1800:
   label = '瞬殺'
@@ -13,7 +13,10 @@ elif sum_seconds <= 10800:
 else:
   label = 'だいぶかかった'
 
-headers = {'Authorization': 'token {}'.format(token)}
-payload = {'labels': [label]}
-r = requests.post(api_url, headers=headers, data=json.dumps(payload))
-print(r)
+with open(environ.get('GITHUB_EVENT_PATH')) as f:
+  events = json.load(f)
+  headers = {'Authorization': 'token {}'.format(token)}
+  payload = {'labels': [label]}
+  api_url = events['issue']['url'] + '/labels'
+  r = requests.post(api_url, headers=headers, data=json.dumps(payload))
+  print(r)
