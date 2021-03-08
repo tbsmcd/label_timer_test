52b622c881ce10603887d50c4f2d9d4aea872e13
tbsmcd <tbsmcd@gmail.com>
2021-03-09 01:08:33 +0900

    Update add_time_label.py

--- .github/add_time_label.py
+++ .github/add_time_label.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import urllib.parse
 import requests
 
 args = sys.argv
@@ -16,6 +17,13 @@ else:
   label = 'だいぶかかった'
 
 headers = {'Authorization': 'token {}'.format(token)}
+api_url = base_url + '/labels/' + urllib.parse.quote('瞬殺')
+requests.delete(api_url, headers=headers)
+api_url = base_url + '/labels/' + urllib.parse.quote('ちょっと厳しい')
+requests.delete(api_url, headers=headers)
+api_url = base_url + '/labels/' + urllib.parse.quote('だいぶかかった')
+requests.delete(api_url, headers=headers)
+
 payload = {'labels': [label]}
 api_url = base_url + '/labels'
 r = requests.post(api_url, headers=headers, data=json.dumps(payload))
